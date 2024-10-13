beaf3c2219aa757d2904c70fb528735639ab94c1
Xime <xebertz@campus.ungs.edu.ar>
2024-10-13 17:27:40 -0300

    Fix model

--- primera-parte/distribuidor_archivos.py
+++ primera-parte/distribuidor_archivos.py
@@ -14,12 +14,6 @@ def distribuir_archivos(capacidad_discos, nombres_archivos, tamaños_archivos):
     print(obj)
 
 
-
-
-
-
-
-
 '''
 d = capacidad de los discos
 f_{i} = capacidad del archivo i CONSTANTE (input) - n archivos
@@ -31,11 +25,11 @@ minimize    y_1 + y_2 + ... + y_n
 s.t 
     1. que no se pasen de capacidad los discos
 
-    x_{1, 1} * a_{1} + x_{2, 1} * a_{2} + ... + x_{n, 1} * a_{1} <= d_{1} * y_{1}
-    x_{1, 2} * a_{1} + x_{2, 2} * a_{2} + ... + x_{n, 2} * a_{2} <= d_{2} * y_{2}
-    x_{1, 3} * a_{1} + x_{2, 3} * a_{2} + ... + x_{n, 3} * a_{3} <= d_{3} * y_{3}
+    x_{1, 1} * f_{1} + x_{2, 1} * f_{2} + ... + x_{n, 1} * f_{1} <= d * y_{1}
+    x_{1, 2} * f_{1} + x_{2, 2} * f_{2} + ... + x_{n, 2} * f_{2} <= d * y_{2}
+    x_{1, 3} * f_{1} + x_{2, 3} * f_{2} + ... + x_{n, 3} * f_{3} <= d * y_{3}
         (. . .)
-    x_{1, j} * a_{1} + x_{2, j} * a_{2} + ... + x_{n, k} * a_{n} <= d_{k} * y_{k}
+    x_{1, j} * f_{1} + x_{2, j} * f_{2} + ... + x_{n, k} * f_{n} <= d * y_{k}
 
     2. que los archivos se elijan solo para un disco
 
@@ -48,7 +42,7 @@ s.t
     3. generales
 
     x_{i, j}, y_{j} pert {0, 1}
-    d_{j}, a_{i} >= 0
+    d, f_{i} >= 0
 
 '''
 
