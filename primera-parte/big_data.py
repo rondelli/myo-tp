95d3ba7855647bbe582168d39f0cb36147964b62
Xime <xebertz@campus.ungs.edu.ar>
2024-10-14 12:35:12 -0300

    Refactor

---
+++ primera-parte/big_data.py
@@ -0,0 +1,47 @@
+from pyscipopt import Model
+from configuracion import generardor_output
+
+def distribuir_archivos(d_t, F, s):
+    model = Model("big_data")
+    d = d_t * 10**6
+
+    # esto no es una constraint del modelo en sí
+    if d < 0 or any(s_i < 0 for s_i in s):
+        return
+
+    n = len(F)
+
+    # esto está medio de más...
+    m = n   # No se puede tener más discos que archivos
+
+    # y_{j} = 1 si se elige el disco j, 0 si no
+    y = [model.addVar(f"y_{j}", vtype="BINARY") for j in range(m)]
+
+    model.setObjective(sum(y), sense="minimize")
+
+    # x_{i, j} = 1 si se elige el archivo i para el disco j, 0 si no
+    x = {}
+    for i in range(m):
+        for j in range(m):
+            x[i, j] = model.addVar(f"x_{i}_{j}", vtype="BINARY")
+
+    # que los archivos se elijan solo para un disco
+    for i in range(n):
+        model.addCons(sum(x[i, j] for j in range(m)) == 1)
+
+    # que no se pasen de capacidad los discos
+    for j in range(m):
+        model.addCons(sum(x[i, j] * s[i] for i in range(n)) <= d * y[j])
+
+    # si el disco no tiene archivos, no puede haber sido elegido
+    for j in range(m):
+        model.addCons(sum(x[i, j] for i in range(n)) <= n * y[j])
+
+    model.optimize()
+    sol = model.getBestSol()
+
+    if sol is not None and model.getStatus() == "optimal" or model.getStatus() == "feasible":
+        generardor_output.generar_output("a_1.out", F, model, y, x, s)
+    else:
+        generardor_output.generar_output_fallido("a_1.out")
+ 
--- primera-parte/main.py
+++ primera-parte/main.py
@@ -2,11 +2,11 @@
 
 from configuracion import generador_configuracion
 from configuracion import leer_configuracion
-import distribuidor_archivos
+import big_data
 
 # esto deberia recibirlo por parametro? desde linea de comandos
 nombre_archivo_config = "a_1.in"
 generador_configuracion.generar_configuracion(nombre_archivo_config)
 capacidad_disco, nombres_archivos, tamaños_archivos = leer_configuracion.leer_configuracion(f"./{nombre_archivo_config}")
-distribuidor_archivos.distribuir_archivos(capacidad_disco, nombres_archivos, tamaños_archivos)
+big_data.distribuir_archivos(capacidad_disco, nombres_archivos, tamaños_archivos)
 
