1ce38cb4adce58898ef2a3c01105e8416758422f
Xime <xebertz@campus.ungs.edu.ar>
2024-10-14 12:55:10 -0300

    Add use of param

--- primera-parte/param.py
+++ primera-parte/param.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
+from configuracion import generador_configuracion
+from configuracion import leer_configuracion
+import big_data
 
 if len(sys.argv) != 2:
-    print(f"Usage: {sys.argv[0]} a_1.in")
+    print(f"Usage: {sys.argv[0]} filename")
     sys.exit(1)
 
-print(sys.argv[1])
+file = sys.argv[1]
+print(f"Using {file}\n")
+
+generador_configuracion.generar_configuracion(sys.argv[1])
+capacidad_disco, nombres_archivos, tamaños_archivos = leer_configuracion.leer_configuracion(f"./{file}")
+
+print(f"Params:\n"
+      f"Disks capacity: {capacidad_disco} TB\n"
+      f"Amount of disks: {len(nombres_archivos)}\n"
+      f"Files: \n" + "\n".join([f"    {nombre}: {tamaño}" for nombre, tamaño in zip(nombres_archivos, tamaños_archivos)]))
+print()
+
+big_data.distribuir_archivos(capacidad_disco, nombres_archivos, tamaños_archivos)
