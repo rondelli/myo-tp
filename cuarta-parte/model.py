b848e8136bb0d0a1288519a41c42167230f99893
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-10-27 00:59:51 -0300

    WIP: file sizes should be less than file_names

--- cuarta-parte/model.py
+++ cuarta-parte/model.py
@@ -5,22 +5,26 @@ import sys
 import os
 import random
 
-########################################################################
-# WIP
-########################################################################
 
 # Functions
-def generate_files(number_of_files : int):
+# Kinda done, check numbers
+def generate_files(number_of_files: int):
+    sizes = []
+    for i in range(random.randrange(10, 20)):
+        sizes.append(random.randint(10, 70) * 10**6)
+
     files = {}
     k = 1
     for i in range(number_of_files):
-        files["file_id_" + str(k)] = random.randint(1000000, 10000000)
+        files["file_id_" + str(k)] = random.choice(sizes)
         k += 1
     return files
 
+
+# Kinda done, check numbers
 def generate_input_file(input_file_name):
-    disk_size = random.randint(1, 100)
-    number_of_files = random.randint(1, 50)
+    disk_size = random.randint(70, 100)
+    number_of_files = random.randint(7, 70)
     files = generate_files(number_of_files)
 
     path_in = os.path.join(os.path.dirname(__file__), ".", "IN", input_file_name)
@@ -33,7 +37,9 @@ def generate_input_file(input_file_name):
         for f in files:
             f_in.write(f + " " + str(files[f]) + "\n")
 
-def read_input_file(input_file_name : str):
+
+# FIXME: file_sizes
+def read_input_file(input_file_name: str):
     disk_size = 0
     file_names = []
     file_sizes = []
@@ -50,18 +56,24 @@ def read_input_file(input_file_name : str):
 
     return disk_size, file_names, file_sizes
 
+
 # d_t: disk size in TB
 # F: file names
-# s: File sizes in MB
-def solve(d_t : int, F : list[str], s : list[int]):
+# S: File sizes in MB
+# WIP
+def solve(d_t: int, F: list[str], S: list[int]):
     if d_t < 0 or any(i < 0 for i in s):
         return
 
     # Tamaño del disco en MB
-    d = d_t * 10**6;
+    d = d_t * 10**6
 
     # Cantidad de archivos
     n = len(F)
+
+    # Cantidad de tamaños de archivos
+    S = list(dict.fromkeys(S))
+    q = len(S)
 
     # Cantidad de discos, a lo sumo, un disco por archivo
     m = n
@@ -69,30 +81,31 @@ def solve(d_t : int, F : list[str], s : list[int]):
     # Define model
     model = Model("big_data")
 
-    # x[<i, j> in F * D] binary: 1 si el archivo $i$ está en el disco $j$, 0 en caso contrario
-    x = {}
-    for i in range(n):
+    # c[k, j] integer: cantidad de archivos de tamaño $k$ que entran en el disco $j$
+    c = {}
+    for k in range(q):
         for j in range(m):
-            x[i, j] = model.addVar(vtype='B', name = f"x_{i}_{j}")
+            c[k, j] = model.addVar(vtype='I', name=f"c_{k}_{j}")
 
-    # y[D] binary: 1 si se usa el disco $j$, 0 en caso contrario
-    y = [model.addVar(vtype='B', name = f"y_{j}") for j in range(m)]
+    # y[j] binary: 1 si se usa el disco $j$, 0 en caso contrario
+    y = [model.addVar(vtype='B', name=f"y_{j}") for j in range(m)]
 
     # minimize disks:
-    model.setObjective(quicksum(y), sense = "minimize")
+    model.setObjective(quicksum(y), sense="minimize")
 
-    # Cada archivo debe estar en un solo disco
-    for i in range(n):
-        model.addCons(quicksum(x[i, j] for j in range(m)) == 1) #, name = "c1_{i}_{j}")
-
-    # Los archivos $i$ que entran en el disco $j$
+    # No pueden haber discos usados con cero cantidad de archivos
     for j in range(m):
-        model.addCons(quicksum(s[i] * x[i, j] for i in range(n)) <= d * y[j]) #, name = "c2_{i}_{j}")
+        model.addCons(quicksum(
+            c[k, j] for k in range(q)) >= 1)
+
+    # Cantidad archivos de tamaño $k$ que entran en el disco $j$
+    for j in range(m):
+        model.addCons(quicksum(s[k] * c[k, j] for k in range(q)) <= d * y[j])
 
     # Esta restricción no es necesaria.
     # No se puede elegir un disco vacío
     # for j in range(m):
-        # model.addCons(quicksum(x[i, j] for i in range(n)) <= n * y[j]) #, name = "c3_{i}_{j}")
+    # model.addCons(quicksum(x[i, j] for i in range(n)) <= n * y[j]) #, name = "c3_{i}_{j}")
 
     model.optimize()
     objective_value = model.getObjVal()
@@ -103,6 +116,7 @@ def solve(d_t : int, F : list[str], s : list[int]):
     else:
         return None
 
+
 def generate_file_output(output_file_name, solution):
     F = solution[0]
     model = solution[1]
@@ -111,19 +125,22 @@ def generate_file_output(output_file_name, solution):
     s = solution[4]
 
     number_of_files = len(F)
-    number_of_disks = number_of_files # La cantidad de discos disponibles es a lo sumo la cantidad de archivos
+    number_of_disks = number_of_files  # La cantidad de discos disponibles es a lo sumo la cantidad de archivos
     number_of_used_disks = round(float(model.getObjVal()))
 
-    path_out = os.path.join(os.path.dirname(__file__), ".", "OUT", output_file_name)
+    path_out = os.path.join(os.path.dirname(__file__), ".", "OUT",
+                            output_file_name)
     with open(path_out, "w") as f_out:
-        f_out.write(f"Para la configuracion del archivo, {number_of_used_disks} discos son suficientes.\n")
+        f_out.write(
+            f"Para la configuracion del archivo, {number_of_used_disks} discos son suficientes.\n"
+        )
         for j in range(number_of_disks):
             if model.getVal(y[j]) == 0:
                 continue
 
             archivos_en_disco = []
             used_space = 0
-            
+
             for i in range(number_of_files):
                 if model.getVal(x[i, j]) == 0:
                     continue
@@ -135,10 +152,12 @@ def generate_file_output(output_file_name, solution):
             for f in archivos_en_disco:
                 f_out.write(f + "\n")
 
+
 def generate_output_file_failed(output_file_name):
     path_out = os.path.join(os.path.dirname(__file__), ".", "OUT", output_file_name)
     with open(path_out, "w") as f_out:
         f_out.write(f"No se ha encontrado solucion para la configuracion del archivo.\n")
+
 
 # Main
 # ----
@@ -154,9 +173,13 @@ generate_input_file(input_file_name)
 
 disk_size, file_names, file_sizes = read_input_file(f"./{input_file_name}")
 
+print(f"d: {disk_size}\nnames: {file_names}\nsizes: {file_sizes}")
+
+"""
 solution = solve(disk_size, file_names, file_sizes)
 
 if solution is not None:
     generate_file_output(f"{input_file_name[:-3]}.out", solution)
 else:
     generate_output_file_failed(f"{input_file_name[:-3]}.out")
+"""
