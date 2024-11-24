b88c92752dd12f7a085e1f26f32c23d24aead2af
Xime <xebertz@campus.ungs.edu.ar>
2024-11-24 18:15:09 -0300

    Minor fixes

--- quinta-parte/set_selector.py
+++ quinta-parte/set_selector.py
@@ -4,10 +4,6 @@ from pyscipopt import SCIP_PARAMSETTING
 # Función para seleccionar conjuntos dados los archivos y conjuntos
 def elegir_conjuntos(F: list, H: list):
     model = Model("set_selector")
-
-    model.setPresolve(SCIP_PARAMSETTING.OFF)
-    model.setHeuristics(SCIP_PARAMSETTING.OFF)
-    model.disablePropagation()
     
     n = len(F)  # cantidad de archivos
     m = len(H)  # cantidad de conjuntos
@@ -26,29 +22,31 @@ def elegir_conjuntos(F: list, H: list):
     model.setObjective(sum(x[j] for j in range(m)), sense="minimize")
 
     # Todos los archivos deben estar en al menos un conjunto elegido
-    constraints = []
     for i in range(n):
         cons = model.addCons(sum(y[i, j] * x[j] for j in range(m)) >= 1)
-        constraints.append(cons)
 
-    # Optimizar el modelo
+    # Desactivación temporal de presolve
+    model.setPresolve(SCIP_PARAMSETTING.OFF)
+    model.disablePropagation()  # esto parece ser la clave para que obj(dual) = obj(primal)
+
     model.optimize()
+
+    # y*
+    solucion_dual = [model.getDualSolVal(c) for c in model.getConss()]
+
+    # Activación de presolve
+    model.setPresolve(SCIP_PARAMSETTING.DEFAULT)
+
     sol = model.getBestSol()
     
     if sol is not None and (model.getStatus() == "optimal" or model.getStatus() == "feasible"):
         # x*
         conjuntos_seleccionados = [j for j in range(m) if model.getVal(x[j]) > 0.5]
 
-        # y*
-        solucion_dual = [model.getDualSolVal(c) for c in model.getConss()]
-
         primal_obj_value = model.getObjVal()
         dual_obj_value = sum(solucion_dual) # rhs es 1 para todas las restricciones
 
         print("\nSol>>>")
-        print("Primal:", primal_obj_value)
-        print("Dual:", dual_obj_value)
-        print("Gap: {:.2%}".format(abs(primal_obj_value - dual_obj_value) / primal_obj_value))
 
         print("x*: ", conjuntos_seleccionados)
         print("y*: ", solucion_dual)
@@ -56,7 +54,7 @@ def elegir_conjuntos(F: list, H: list):
         print("dual obj: ", dual_obj_value)
 
         if abs(dual_obj_value - primal_obj_value) < 1e-6:
-            print("¡Dualidad fuerte confirmada!")
+            print("Los valores objetivo coinciden")
         else:
             print("Los valores objetivo del primal y del dual no coinciden.")
 
@@ -64,13 +62,3 @@ def elegir_conjuntos(F: list, H: list):
     else:
         print("No se encontró una solución factible.")
         return None
-
-# Ejemplo de uso
-F = ["archivo1", "archivo2", "archivo3", "archivo4", "archivo5"]
-H = [
-    {"archivo1", "archivo2"},
-    {"archivo2", "archivo3"},
-    {"archivo1", "archivo3", "archivo4", "archivo5"},
-]
-
-#elegir_conjuntos(F, H)
