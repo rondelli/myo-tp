284f87c36140682ec1d0509ba9ee29db1582ef4f
Xime <xebertz@referentesa.com.ar>
2024-11-25 14:42:56 -0300

    Refactor -> csv creation to function

--- 7 - entrega/generador-csv.py
+++ 7 - entrega/generador-csv.py
@@ -1,20 +1,19 @@
 import csv
 import os
 
-headers = ["caso", "cant", "cota dual", "mejor1", "var1", "tiempo1", "mejor4", "var4", "tiempo4", "mejor5", "var5", "tiempo5", "mejor6", "var6", "tiempo6"]
-datos = []
+def guardar_prueba(datos):
+    headers = ["caso", "cant", "cota dual", "mejor1", "var1", "tiempo1", "mejor4", "var4", "tiempo4", "mejor5", "var5", "tiempo5", "mejor6", "var6", "tiempo6"]
 
-ruta_actual = os.path.dirname(os.path.abspath(__file__)) 
-nombre_archivo = "pruebas.csv" 
-ruta_archivo = os.path.join(ruta_actual, nombre_archivo)
+    ruta_actual = os.path.dirname(os.path.abspath(__file__)) 
+    nombre_archivo = "pruebas.csv" 
+    ruta_archivo = os.path.join(ruta_actual, nombre_archivo)
 
-fue_generado = os.path.isfile(ruta_archivo)
+    fue_generado = os.path.isfile(ruta_archivo)
 
-with open(ruta_archivo, 'a', newline='') as archivo:
-    archivo_csv = csv.writer(archivo)
+    with open(ruta_archivo, 'a', newline='') as archivo:
+        archivo_csv = csv.writer(archivo)
 
-    if not fue_generado:
-        archivo_csv.writerow(headers)
-    
-    # Escribir las filas en el archivo CSV
-    archivo_csv.writerows(datos)
+        if not fue_generado:
+            archivo_csv.writerow(headers)
+        
+        archivo_csv.writerows(datos)
