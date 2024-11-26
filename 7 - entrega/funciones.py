e8eb609a1dfaa8b7acdd84ab5a0f0012fb5f46f4
Xime <xebertz@referentesa.com.ar>
2024-11-25 22:51:10 -0300

    Add prueba archivo config

--- 7 - entrega/funciones.py
+++ 7 - entrega/funciones.py
@@ -28,7 +28,6 @@ def leer_configuracion():
         for linea in archivo:
             if '=' in linea:
                 clave, valor = linea.strip().split('=', 1)
-
                 valor = valor.strip().strip('\'"')
                 configuraciones[clave] = valor
     
--- 7 - entrega/prueba.py
+++ 7 - entrega/prueba.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
 from funciones import *
+import glob
 
 # 1 - leer config
 configuraciones = leer_configuracion()
 print(configuraciones)
+
+inPath = configuraciones.get('inPath').replace('/', os.sep)
+outPath = configuraciones.get('outPath').replace('/', os.sep)
+threshold = int(configuraciones.get('threshold', 0))
+archivos = os.listdir(inPath)
+archivos = [f for f in archivos if os.path.isfile(f)]
+
+# Imprimir los nombres de los archivos
+for archivo in archivos:
+    print(archivo)
+
 
 # 2 - para todo archivo dentro del directorio, ejecutar los 4 modelos
 
