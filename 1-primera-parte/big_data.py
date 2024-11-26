e82691e89827bfc1b508e905b14dfe0056b1dce3
Lucifer! <6011234+rondelli@users.noreply.github.com>
2024-11-26 13:05:58 -0300

    Add model_part_1 {primal, dual}

--- 1-primera-parte/big_data.py
+++ 1-primera-parte/big_data.py
@@ -1,62 +1,118 @@
 import time
 import sys
 from pyscipopt import Model
+from pyscipopt import quicksum
+from pyscipopt import SCIP_PARAMSETTING
 
+# Para que no se rompa lo viejo, aunque me parece que lo rompe
+# No usar esta función
 def distribuir_archivos(d_t, F, s, time_limit=420):
-    """
-    Distribuye archivos entre discos minimizando el número de discos utilizados.
-    
-    Parámetros:
-    - d_t: Capacidad total de cada disco en MB.
-    - F: Lista de nombres de archivos.
-    - s: Lista de tamaños de archivos correspondientes a cada archivo en MB.
-    - time_limit: Tiempo límite en segundos para encontrar la mejor solución posible.
+    modelo = crear_modelo1(d_t, F, s, time_limit)
+    x, obj_x = obtener_solucion_primal1(modelo)
+    y, obj_y = obtener_solucion_dual(modelo)
 
-    Retorna:
-    - Una lista con la mejor solución encontrada dentro del tiempo límite o None si no se encontró ninguna.
-    """
-    model = Model("big_data")
+    sys.stderr.write(f"[Debugging] obj x {obj_x}\n")
+    sys.stderr.write(f"[Debugging] obj y {obj_y}\n")
+
+# Crea el modelo y lo devuelve optimizado
+def crear_modelo1(d_t: int, F: list[str], s: list[int], time_limit=420):
+    model = Model("model_part_1")
 
     d = d_t * 10**6
-    if d < 0 or any(s_i < 0 for s_i in s):
-        return None
+
+    # living on the edge
+    # if d < 0 or any(s_i < 0 for s_i in s):
+        #return None
 
     n = len(F)
     m = n  # no se puede tener más discos que archivos
 
+    ##################################################################
+    # BINARY
+    # x_{i, j} = 1 si se elige el archivo i para el disco j, 0 si no
+    #
+    """
+    x = {}
+    for i in range(n):
+        for j in range(m):
+            x[i, j] = model.addVar(name = f"x_{i}_{j}", vtype="BINARY")
+
     # y_{j} = 1 si se elige el disco j, 0 si no
     y = [model.addVar(f"y_{j}", vtype="BINARY") for j in range(m)]
-    model.setObjective(sum(y), sense="minimize")
+    """
+    #
+    ##################################################################
 
+    ##################################################################
+    # CONTINUOUS
     # x_{i, j} = 1 si se elige el archivo i para el disco j, 0 si no
+    #
     x = {}
-    for i in range(m):
+    for i in range(n):
         for j in range(m):
-            x[i, j] = model.addVar(f"x_{i}_{j}", vtype="BINARY")
+            x[i, j] = model.addVar(name = f"x_{i}_{j}", lb=0, ub=1, vtype="CONTINUOUS")
+
+    # y_{j} = 1 si se elige el disco j, 0 si no
+    y = [model.addVar(f"y_{j}", lb=0, ub=1, vtype="CONTINUOUS") for j in range(m)]
+    #
+    ##################################################################
+
+    # minimize disks:
+    model.setObjective(quicksum(y), sense="minimize")
 
     # Que los archivos se elijan solo para un disco
     for i in range(n):
-        model.addCons(sum(x[i, j] for j in range(m)) == 1)
+        model.addCons(quicksum(x[i, j] for j in range(m)) == 1)
 
     # Que no se pasen de capacidad los discos
     for j in range(m):
-        model.addCons(sum(x[i, j] * s[i] for i in range(n)) <= d * y[j])
+        model.addCons(quicksum(x[i, j] * s[i] for i in range(n)) <= d * y[j])
 
     # Configurar el límite de tiempo en el solver
     model.setParam("limits/time", time_limit)
-
     model.setParam("display/freq", 1)
 
-    model.optimize()
-    sys.stderr.write(f"[Debuggin] Time: {model.getSolvingTime()}\n\n")
+    # Desactivación temporal de presolve
+    model.setPresolve(SCIP_PARAMSETTING.OFF)
 
+    ####################################################
+    # Esto es la clave para que obj(dual) = obj(primal)
+    # según documentación de pyscipopt
+    #
+    model.disablePropagation()
+    model.setHeuristics(SCIP_PARAMSETTING.OFF)
+    #
+    ####################################################
+
+    model.optimize()
+
+    sys.stderr.write(f"[Debuggin] Time: {model.getSolvingTime()}\n\n")
     sys.stderr.write(f"[Debuggin] Cantidad sols: {model.getNSols()}\n\n")
 
-    # Obtener la mejor solución encontrada
-    sol = model.getBestSol()
+    return model
 
-    if sol is not None and (model.getStatus() == "optimal" or model.getStatus() == "feasible"):
+def obtener_solucion_primal1(model):
+    sol = model.getBestSol()
+    status = model.getStatus()
+
+    if sol is not None and status in ["optimal", "feasible"]:
         sys.stderr.write(f"[Debuggin] {model.getStatus()}: {model.getBestSol()}\n\n")
-        return [F, model, y, x, s]
+
+        # El nombre de variable x, acá es missleading, es x porque es el primal.
+        # Esta línea devuelve **todas** las variables del modelo, las $x$: archivo seleccionado y las $y$: disco seleccionado
+        x = [v.getLPSol() for v in model.getVars(False)]
+
+        sys.stderr.write(f"[Debugging]: todas las variables del primal model_part_1 {x}\n\n")
+        return x, model.getObjVal()
     else:
         return None
+
+def obtener_solucion_dual(model):
+    # No debería ser necesario si el modelo ya viene optimizado con el presolving off
+    # Aseguarse de apagar el presolving
+    # model.setPresolve(SCIP_PARAMSETTING.OFF)
+
+    # La longitud de y coincide con la cantidad de archivos del input
+    y = [model.getDualSolVal(c) for c in model.getConss(False)]
+
+    return y, sum(y)
