2dc69bdce6f0d23f7cce10dfd21a03093b2e5fbc
Lucia Soria <luciacsoria5@gmail.com>
2024-11-27 00:57:39 -0300

    fix

--- 7_entrega/prueba.py
+++ 7_entrega/prueba.py
@@ -35,17 +35,27 @@ sys.stderr.write(f"[Debugging] {archivos}\n")
 for a in archivos:
     sys.stderr.write(f"[Debugging] {a}\n")
     d_t, F, s = leer_archivo(a)
-    _, modelo_1, _, _, _ = model_part_1.distribuir_archivos_1(d_t, F, s, threshold*60)
+    datos = model_part_1.distribuir_archivos_1(d_t, F, s, threshold*60)
     modelo_4 = model_part_4.distribuir_archivos_4(d_t, F, s, 11, threshold*60)
     modelo_5  = model_part_5.obtener_conjuntos(a, threshold*60)
         
     caso = a
     cant = len(F)
 
-    cota_dual_1, mejor_1, var_1, tiempo_1 = datos_modelo(modelo_1)
-    cota_dual_4, mejor_4, var_4, tiempo_4 = datos_modelo(modelo_4[1])
+    if datos is not None:
+        cota_dual_1, mejor_1, var_1, tiempo_1 = datos_modelo(datos[1])
+    if modelo_4 is not None:
+        cota_dual_4, mejor_4, var_4, tiempo_4 = datos_modelo(modelo_4[1])
+
     cota_dual_5, mejor_5, var_5, tiempo_5 = datos_modelo(modelo_5)
 
-    cota_dual = max([cota_dual_1, cota_dual_4])
+    # cota_dual = max([cota_dual_1, cota_dual_4, cota_dual_5])
+    cotas = [cota_dual_1, cota_dual_4, cota_dual_5]
+    cotas_validas = [c for c in cotas if c is not None]
+
+    if cotas_validas:
+        cota_dual = max(cotas_validas)
+    else:
+        cota_dual = None
 
     guardar_prueba([[caso, cant, cota_dual, mejor_1, var_1, tiempo_1, mejor_4, var_4, tiempo_4, mejor_5, var_5, tiempo_5, 0, 0, 0]])
