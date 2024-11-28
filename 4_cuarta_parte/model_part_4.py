9112cc6fac2930d2b48c0d10a93d4c2d416f80d0
Xime <xebertz@referentesa.com.ar>
2024-11-28 20:18:14 -0300

    Fix modelo 4

--- 4_cuarta_parte/model_part_4.py
+++ 4_cuarta_parte/model_part_4.py
@@ -50,15 +50,6 @@ def distribuir_archivos_4(d_t, F, S, t, time_limit=420):
     # Configurar el límite de tiempo en el solver
     #model.setParam("display/freq", 1)
 
-   # model.setHeuristics(SCIP_PARAMSETTING.AGGRESSIVE)
-    #model.setHeuristics(SCIP_PARAMSETTING.FAST) # Parece más rápido
-
-    #model.setEmphasis(SCIP_PARAMEMPHASIS.EASYCIP)
-    #model.setParam("parallel/maxnthreads", 16)
-
-    # Parece no afectar
-    # model.setParam("parallel/mode", 0) # 0: opportunistic or 1: deterministic. [1]
-
     model.optimize()
 
     sys.stderr.write(f"[Debugging] [MODELO 4] Time: {model.getSolvingTime()}\n\n")
