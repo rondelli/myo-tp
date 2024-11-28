f9702aed37e7a43aab7e8184fd0528a9c897b096
Xime <xebertz@referentesa.com.ar>
2024-11-28 19:55:25 -0300

    Fix modelo 1

--- 1_primera_parte/model_part_1.py
+++ 1_primera_parte/model_part_1.py
@@ -61,15 +61,6 @@ def resolver_modelo_binario_1(d_t: int, F: list[str], s: list[int], time_limit=4
     model.setParam("limits/time", time_limit)
     #model.setParam("display/freq", 1)
 
-    #model.setHeuristics(SCIP_PARAMSETTING.AGGRESSIVE)
-    #model.setHeuristics(SCIP_PARAMSETTING.FAST) # Parece más rápido
-
-    #model.setEmphasis(SCIP_PARAMEMPHASIS.EASYCIP)
-    #model.setParam("parallel/maxnthreads", 16)
-
-    # Parece no afectar
-    # model.setParam("parallel/mode", 0) # 0: opportunistic or 1: deterministic. [1]
-
     model.optimize()
 
     sys.stderr.write(f"[Debugging] [MODELO 1] Time: {model.getSolvingTime()}\n\n")
