ec38db404b1ac188f604ce1a54685cedaacc3e53
Xime <xebertz@campus.ungs.edu.ar>
2024-12-18 23:30:53 -0300

    Add test suite for Pattern

--- 4_cuarta_parte/Pattern.py
+++ 4_cuarta_parte/Pattern.py
@@ -10,6 +10,10 @@ def obtener_patrones(capacidad_maxima, tamaños):
         tamaño = tamaños[indice]
         maximo = espacio_disponible // tamaño
         
+        # viendolo al revés ⟶ range(maximo, 0, -1),
+        # quizás podamos hacer que busque maximales directamente
+        # aunque una vez le pregunté si había que generar solo maximales
+        # (a mano, en los ejercicios de patrones), y me dijo que no hacía falta
         for cont in range(maximo + 1):
             patron_actual[indice] = cont
             obtener_patron(espacio_disponible - cont * tamaño, indice + 1, patron_actual)
---
+++ 4_cuarta_parte/TestPattern.py
@@ -0,0 +1,89 @@
+import unittest
+import Pattern
+
+class TestPattern(unittest.TestCase):
+    def test_capacidad_0(self):
+        capacidad_maxima = 0
+        tamaños = range(10)
+        patrones_esperados = [[0] * 10]
+
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+
+    def test_capacidad_menor_a_tamaños(self):
+        capacidad_maxima = 2
+        tamaños = range(3, 10)
+        patrones_esperados = [[0] * 7]
+        
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+
+    def test_tamaños_0(self):
+        capacidad_maxima = 2
+        tamaños = [0] * 2
+
+        # Acá esperamos que meta infinitas veces cada tamaño... no creo que haga falta
+        # patrones_esperados = [[0] * 7]
+        
+        # patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        # self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+    
+    def test_tamaño_igual_que_capacidad(self):
+        capacidad_maxima = 2
+        tamaños = [2] * 2
+
+        patrones_esperados = [[1, 0],
+                              [0, 1]]
+        
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+
+    def test_tamaños_redondos(self):    #jasashjasjas no se como ponerle
+        capacidad_maxima = 10
+        tamaños = [3, 7]
+
+        patrones_esperados = [[1, 1]]
+        
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+    
+    def test_tamaños_diferencia(self):    #jasashjasjas no se como ponerle
+        capacidad_maxima = 10
+        tamaños = [3, 5]
+
+        patrones_esperados = [[0, 2],
+                              [1, 1],
+                              [3, 0]]
+        
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+        self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+
+    def test_tres_tamaños(self):
+        capacidad_maxima = 10
+        tamaños = [1, 2, 3]
+
+        patrones_esperados = [[1, 0, 3],
+                              [2, 1, 2],
+                              [4, 0, 2],
+                              [1, 3, 1],
+                              [3, 2, 1],
+                              [5, 1, 1],
+                              [7, 0, 1],
+                              [0, 5, 0],
+                              [2, 4, 0],
+                              [4, 3, 0],
+                              [6, 2, 0],
+                              [8, 1, 0],
+                              [10, 0, 0]]
+
+        patrones_obtenidos = Pattern.obtener_patrones(capacidad_maxima, tamaños)
+
+        # la idea es que haga solo los que necesita
+        patrones_esperados_set = set(map(tuple, patrones_esperados))
+        patrones_obtenidos_set = set(map(tuple, patrones_obtenidos))
+
+        self.assertTrue(patrones_esperados_set.issubset(patrones_obtenidos_set), "No todos los patrones están en 'patrones_obtenidos_set'.")
+
+        # self.assertEqual(patrones_esperados, patrones_obtenidos, msg=None)
+
+unittest.TextTestRunner().run(unittest.makeSuite(TestPattern))
