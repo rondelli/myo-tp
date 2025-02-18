d3822962622233974432b1ee6049913f9d9f1e32
Xime <xebertz@campus.ungs.edu.ar>
2025-02-17 21:37:19 -0300

    Add ejemplo Agus Vidal

--- 5_quinta_parte/generacion-conjuntos.py
+++ 5_quinta_parte/generacion-conjuntos.py
@@ -29,6 +29,31 @@ while sum(tamaños_cantidades.values()) > 0:
 
 print(patrones)
 
+# FUNCION DE AGUS
+def generar_subconjuntos(peso_disco, nombres_archivos, pesos_archivos, tiempo_inicio, tiempo_limite_total):
+            H = []
+            archivos = list(zip(nombres_archivos, pesos_archivos))
+            print ("Generando subconjuntos")
+            if not chequear_existe_tiempo_ejecucion(tiempo_inicio, tiempo_limite_total):
+                print("Se terminó el tiempo generando los conjuntos.")
+                return None
+            # También generamos subconjuntos de archivos de forma más interdependiente para agregar variedad
+            for i in range(len(archivos)):
+                for j in range(i + 1, len(archivos)):
+                    # Intentamos agregar combinaciones de diferentes pares de archivos
+                    combo = [archivos[i], archivos[j]]
+                    total_size = sum(peso for _, peso in combo)
+                    if not chequear_existe_tiempo_ejecucion(tiempo_inicio, tiempo_limite_total):
+                        print("Se terminó el tiempo generando los subconjuntos.")
+                        return []
+                    if total_size <= peso_disco:
+                        H.append([nombre for nombre, _ in combo])
+            if not chequear_existe_tiempo_ejecucion(tiempo_inicio, tiempo_limite_total):
+                        print("Se terminó el tiempo generando los subconjuntos.")
+                        return []
+            print ("Termino de generar subconjuntos")
+            return H
+
 
 
 
