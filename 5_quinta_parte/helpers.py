0ece7d26946505e283c6637675dc18dfc9038347
Xime <xebertz@campus.ungs.edu.ar>
2025-03-08 16:16:29 -0300

    Fix generar_subconjuntos

--- 5_quinta_parte/helpers.py
+++ 5_quinta_parte/helpers.py
@@ -12,11 +12,10 @@ def generar_subconjuntos_5(peso_disco, nombres_archivos, tamaños_archivos):
         for j in range(i + 1, len(archivos)):
             combo = [archivos[i], archivos[j]]
             total_size = sum(peso for _, peso in combo)
-
+            
             if total_size <= peso_disco:
                 H.append(set([nombre for nombre, _ in combo]))
                 agregado = True
-                
         if not agregado:
             H.append(set([archivos[i][0]]))
     return H
@@ -45,25 +44,23 @@ def generar_subconjuntos_6(tamaño_disco, nombres_archivos, tamaños_archivos):
 
     return H
 
-def generar_subconjuntos(tamaño_disco, archivos):
-    patrones = []
-    peso_minimo = min(archivos.values())
+def generar_subconjuntos(tamaño_disco, nombres_archivos, tamaños_archivos):
+    H = []
+    archivos = list(zip(nombres_archivos, tamaños_archivos))
 
     for r in range(1, len(archivos) + 1):
-        for combinacion in combinations_with_replacement(archivos.values(), r):
-            
-            tamaño_combinacion = sum(tamaño for tamaño in combinacion)
-            if tamaño_combinacion <= tamaño_disco and tamaño_disco - tamaño_combinacion < peso_minimo:
-                patron = {}
-                
-                for tamaño in combinacion:
-                    patron[tamaño] = patron.get(tamaño, 0) + 1
-                
-                if patron not in patrones:
-                    patrones.append(patron)
-                # patrones.append([nombre for nombre, _ in combo])
+        for combinacion in combinations_with_replacement(archivos, r):
+            tamaño_combinacion = sum(tamaño for _, tamaño in combinacion)
 
-    return patrones
+            if tamaño_combinacion <= tamaño_disco and tamaño_disco:
+                h = set([nombre for nombre, _ in combinacion])
+                
+                # for _, tamaño in combinacion:
+                #     h[tamaño] = h.get(tamaño, 0) + 1
+                
+                if h not in H:
+                    H.append(h)
+    return H
 
 def obtener_conjuntos_seleccionados(solucion):
     conjuntos_seleccionados = [i for i in range(len(solucion)) if solucion[i] == 1]
