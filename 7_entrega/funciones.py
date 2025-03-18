ee8c6ab30b0c2b45744fc2ec0aeb959800ec00dc
Xime <xebertz@campus.ungs.edu.ar>
2025-03-17 21:43:07 -0300

    Refactor

--- 7_entrega/funciones.py
+++ 7_entrega/funciones.py
@@ -2,7 +2,7 @@ import csv
 import os
 import math
 
-def agregar_contenido_a_linea(caso, nuevos_datos):
+def agregar_contenido_a_archivo(caso, nuevos_datos):
     ruta_actual = os.path.dirname(os.path.abspath(__file__)) 
     nombre_archivo = "pruebas.csv" 
     ruta_archivo = os.path.join(ruta_actual, nombre_archivo)
@@ -18,18 +18,15 @@ def agregar_contenido_a_linea(caso, nuevos_datos):
             break
 
     if not existe_caso:
-        lector_csv.append([caso] + nuevos_datos)   # Agregar los datosa una nueva línea
+        lector_csv.append([caso] + nuevos_datos)   # Agregar los datos a una nueva línea
 
     with open(ruta_archivo, 'w', newline='') as archivo:
         escritor_csv = csv.writer(archivo)
         escritor_csv.writerows(lector_csv)
 
 def guardar_prueba(resultado):
-    # resultado = [caso, cant, cota_dual, mejores, var, tiempos]
-    # [caso, cant, cota dual, mejor, var, tiempo, mejor, var, tiempo, mejor, var, tiempo, mejor, var, tiempo]
     resultados = [[resultado[0], resultado[1], resultado[2], resultado[3][0], resultado[4][0], resultado[5][0], resultado[3][1], resultado[4][1], resultado[5][1], resultado[3][2], resultado[4][2], resultado[5][2], resultado[3][3], resultado[4][3], resultado[5][3]]]
     escribir_csv(resultados)
-
 
 def escribir_csv(datos):
     headers = ["caso", "cant", "cota dual", "mejor1", "var1", "tiempo1", "mejor4", "var4", "tiempo4", "mejor5", "var5", "tiempo5", "mejor6", "var6", "tiempo6"]
@@ -50,8 +47,7 @@ def escribir_csv(datos):
 
 def datos_modelo(model):
     try:
-        #_, mejor = model_part_1.obtener_solucion_primal_1(model)
-        mejor = math.ceil(model.getObjVal()) # esto es lo mimso que lo de arriba --> si no encuentra el optimo da 0 :c
+        mejor = math.ceil(model.getObjVal())
         var = math.ceil(model.getNVars())
         tiempo = model.getSolvingTime()
         cota_dual = math.ceil(model.getDualbound())
