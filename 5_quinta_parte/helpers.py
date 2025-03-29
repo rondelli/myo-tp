6cd34111819b5666ff0f7b0ae65ff096d608e964
Xime <xebertz@campus.ungs.edu.ar>
2025-03-29 12:22:08 -0300

    Add time limit in subset generation

--- 5_quinta_parte/helpers.py
+++ 5_quinta_parte/helpers.py
@@ -1,21 +1,24 @@
 import time
 
 # * Esta opcion retorna conjuntos con los nombres de los archivos, por ejemplo [{'a3', 'a5'}, {'a7', 'a4', 'a1'}, {'a6', 'a2'}]
-def generar_subconjuntos(tamaño_disco, nombres_archivos, tamaños_archivos):
+def generar_subconjuntos(tamaño_disco, nombres_archivos, tamaños_archivos, tiempo_limite=420):
+    tiempo_inicio = time.time()
     H = []
     archivos = list(zip(nombres_archivos, tamaños_archivos))
     archivos.sort(key=lambda x: x[1], reverse=True)
 
-    while archivos:
+    while archivos and hay_tiempo(tiempo_inicio, tiempo_limite):
         subconjunto = set()
         espacio_restante = tamaño_disco
 
         for archivo in archivos[:]:
-            if archivo[1] <= espacio_restante:
-                subconjunto.add(archivo[0])
-                espacio_restante -= archivo[1]
-                archivos.remove(archivo)
-
+            if hay_tiempo(tiempo_inicio, tiempo_limite):
+                if archivo[1] <= espacio_restante:
+                    subconjunto.add(archivo[0])
+                    espacio_restante -= archivo[1]
+                    archivos.remove(archivo)
+            else:
+                return None
         H.append(subconjunto)
     return H
 
@@ -48,7 +51,6 @@ def es_optimo(model, variables, solucion, sol):
     for var, val in zip(variables, solucion):
         model.setSolVal(sol, var, val)
     return model.checkSol(sol)
-
 
 def hay_tiempo(tiempo_inicio, tiempo_limite):
     return (time.time() - tiempo_inicio) < tiempo_limite
